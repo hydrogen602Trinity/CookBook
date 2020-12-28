acb8321407aad2d967cb8a52971a6850c611f059
Jonathan Rotter <hydrogen31415@gmail.com>
2020-12-28 18:10:24 +0100

    more work

--- recipe.py
+++ recipe.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 from fractions import Fraction
-from typing import List # List[int]
+import fractions
+from typing import Dict, List, Union # List[int]
 import uuid
 import base64
 import json
@@ -11,6 +12,26 @@ class Ingredient:
         self.name: str = name
         self.unit: str = unit
         self.amount: Fraction = amount
+    
+    @staticmethod
+    def fromJson(data: Union[str, dict]) -> Ingredient:
+        if isinstance(data, str):
+            data = json.loads(data)
+        assert isinstance(data, dict)
+        
+        name = data['name']
+        unit = data['unit']
+        amount = data['amount']
+        assert isinstance(name, str) and name != ''
+        assert isinstance(unit, str) and unit != ''
+        assert isinstance(amount, (str, int, float)) and amount != ''
+        amount = fractions.Fraction(amount)
+        assert amount > 0
+
+        return Ingredient(name, unit, amount)
+    
+    def toJsonSerializable(self) -> Dict[str, object]:
+        return self.__dict__
 
 
 class Recipe:
@@ -23,7 +44,7 @@ class Recipe:
         self.id: str = myId
     
     def toJson(self) -> str:
-        return json.dumps(self.__dict__)
+        return json.dumps(self.__dict__, default=jsonEncoderHelper)
 
     @staticmethod
     def fromJson(jsonStr: str) -> Recipe:
@@ -31,12 +52,19 @@ class Recipe:
         name = temp["recipeName"]
         ingredients = temp["ingredients"]
         instructions = temp["instructions"]
-        notes = temp["notes"]
-        
+        if "notes" in temp:
+            notes = temp["notes"]
+        else:
+            notes = ''
 
         assert isinstance(name, str)
         assert name != ""
         #Need to finish Ingredients 
+        assert isinstance(ingredients, list)
+
+        ingredients = list(map(Ingredient.fromJson, ingredients))
+
+        assert isinstance(instructions, list)
         for i in instructions:
             assert isinstance(i, str)
         assert instructions != []
@@ -44,8 +72,23 @@ class Recipe:
 
         if "id" in temp:
             myId = temp["id"]
-            #Check that id is a base 64 url safe 
+            assert isinstance(myId, str)
+
+            # verify that the id is correct
+            decoded = base64.urlsafe_b64decode(myId.encode())
+            assert len(decoded) == 16
+            assert base64.urlsafe_b64encode(decoded) == myId.encode()
+
             return Recipe(name, ingredients, instructions, notes, myId)
         else:
             return Recipe(name, ingredients, instructions, notes)
 
+
+def jsonEncoderHelper(obj):
+    # find some better way?
+    if isinstance(obj, Ingredient):
+        return obj.toJsonSerializable()
+    elif isinstance(obj, fractions.Fraction):
+        return str(obj)
+    else:
+        raise TypeError
