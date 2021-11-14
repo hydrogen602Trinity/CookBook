ed25b44caee05e499cc7612fdd8968520a6c492d
hydrogen602 <hydrogen31415@gmail.com>
2021-11-13 19:56:46 -0600

    various fixes, fixing up the local repo

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 // import './index.css';
 // import App from './App';
 //import Note from './Note';
 import Recipes from './Recipes';
+import Users from './Users';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Recipes/>
+    <BrowserRouter>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/recipes">Recipes</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+          </nav>
+      </div>
+      <Routes>
+        <Route path="/recipes" element={<Recipes/>}/>
+        <Route path="/users" element={<Users/>}/>
+      </Routes>
+    </BrowserRouter>
+    
   </React.StrictMode>,
   document.getElementById('root')
 );
