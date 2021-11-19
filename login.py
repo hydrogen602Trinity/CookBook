7a9f154a949e0d6bd61a477765247a71ac6c3d44
hydrogen602 <hydrogen31415@gmail.com>
2021-11-18 21:26:18 -0600

    test error fix

--- login.py
+++ login.py
@@ -1,3 +1,4 @@
+from sys import stderr
 from flask_login import LoginManager
 from database import db
 
@@ -9,14 +10,10 @@ def setup(app):
 
     global __has_setup, load_user
     if __has_setup:
-        raise RuntimeError('Initialized login manager twice')
+        print('Initialized login manager twice', file=stderr)
     __has_setup = True
 
     @login_manager.user_loader
     def load_user(user_id: str):
         with app.app_context():
             return db.session.query(User).get(int(user_id))
-            # q = db.session.query(User).filter(User.email == user_id)
-            # if q.count() == 0:
-            #     return None
-            # return q.one()
