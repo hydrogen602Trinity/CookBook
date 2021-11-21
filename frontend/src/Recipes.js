3e15093628dedae43c352067031a4ac236075b5e
hydrogen602 <hydrogen31415@gmail.com>
2021-11-21 01:25:14 -0600

    search recipes ui

--- frontend/src/Recipes.js
+++ frontend/src/Recipes.js
@@ -7,6 +7,10 @@ import CircularProgress from '@material-ui/core/CircularProgress';
 import { useEffect, useState } from 'react';
 import createTrigger from "react-use-trigger";
 import useTrigger from "react-use-trigger/useTrigger";
+import Button from '@mui/material/Button';
+
+import { SearchPopup } from './components/Form';
+
 // import RecipeEditor from './components/RecipeEditor';
 
 
@@ -37,22 +41,47 @@ export default function Recipes() {
 
     console.log(recipes, isLoading);
 
+    const [showSearch, setShowSearch] = useState(false);
+    const [searchTerm, setSearchTerm] = useState('default');
+
+
     return (
         <div className="frame recipes-main">
             <div className="header">
                 <h1>
                     Recipe Book
                 </h1>
-                <button onClick={addRecipe}>
-                    <i className="fas fa-plus"></i>
-                </button>
+                <div className="actions">
+                    <Button className="actions-buttons" onClick={() => setShowSearch(true)}>
+                        <i className="fas fa-search" style={{color: 'black'}}></i>
+                    </Button>
+                    <Button className="actions-buttons" onClick={addRecipe}>
+                        <i className="fas fa-plus" style={{color: 'black'}}></i>
+                    </Button>
+                </div>
             </div>
             {/* <div className="bar" hidden={true}>
                 <button>New</button>
                 <button>Edit</button>
                 <button>Delete</button>
             </div> */}
+            
             <div className="main" id="content">
+                {searchTerm ?
+
+                    <div className="search-term">
+                        <div>
+                            <span>
+                                <Button className="actions-buttons" onClick={() => setShowSearch(true)}>
+                                    <i className="fas fa-search" style={{color: 'black'}}></i>
+                                </Button>{searchTerm}
+                            </span>
+                            <Button onClick={() => setSearchTerm('')}>
+                                <i class="fas fa-times"></i>
+                            </Button>
+                        </div>
+                    </div>
+                : null}
                 {isLoading ? 
                     <CircularProgress className="circular-progress"/>
                 : 
@@ -64,6 +93,11 @@ export default function Recipes() {
                             />)
                 }
             </div>
+            <SearchPopup 
+                show={showSearch} 
+                handleClose={() => setShowSearch(false)}
+                callback={setSearchTerm}
+            />
         </div>
     );
 }
--- frontend/src/components/Form.js
+++ frontend/src/components/Form.js
@@ -13,11 +13,56 @@ import IconButton from '@mui/material/IconButton';
 import InputLabel from '@mui/material/InputLabel';
 import FormControl from '@mui/material/FormControl';
 
-
-
 import "./Form.scss";
 import { useState } from 'react';
 import useBetterState from '../util/classLikeState';
+
+// props need to have
+// show => bool
+// handleClose => fn that hides
+// callback => fn that receives state
+
+export function SearchPopup(props) {
+    const init_state = '';
+
+    const handleClose = props.handleClose;
+    const handleCreate = () => {
+        props.callback(state);
+        handleClose();
+        setState(init_state);
+    };
+
+    const [state, setState] = useState('');
+
+    const handleChange = ev => { setState(ev.target.value); }
+
+    return (
+    <Dialog open={props.show} onClose={handleClose} maxWidth="sm" fullWidth>
+        <DialogTitle>Search</DialogTitle>
+        <DialogContent>   
+            <TextField
+                autoFocus
+                margin="dense"
+                id="search-term"
+                label="Search Term"
+                fullWidth
+                autoComplete={false}
+                variant="outlined"
+                value={state}
+                onChange={handleChange}
+            />
+            {/* <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
+                <SearchIcon sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
+                
+            </Box> */}
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button onClick={handleCreate}>Search</Button>
+        </DialogActions>
+    </Dialog>
+    );
+}
 
 export function NewUser(props) {
     const handleClose = props.handleClose;
@@ -44,8 +89,7 @@ export function NewUser(props) {
         event.preventDefault();
     };
 
-    const handleChange = (field) => (ev) => { setState({[field]: ev.target.value});
-    }
+    const handleChange = (field) => (ev) => { setState({[field]: ev.target.value}); }
 
     return (
         // <div className="center form-component">
