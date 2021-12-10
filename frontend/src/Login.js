40a331738a6bc66bee35cabc254db74cad807059
Vasti Sarai Rios Rios <vriosrio@trinity.edu>
2021-12-09 20:16:16 -0600

    Finished the loggin page

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -4,33 +4,46 @@ import { useFetchAPI } from './util/fetchAPI';
 import Button from '@mui/material/Button';
 import useLogin from './util/login';
 import * as React from 'react';
-
+import "./Theme.scss"
 
 import useBetterState from './util/classLikeState';
 import TextField from '@mui/material/TextField';
 import {
-    BrowserRouter,
-    Routes,
-    Route,
-    Link,
-    useSearchParams
+    useNavigate
   } from "react-router-dom";
 import "./Login.scss";
 
 export default function Login(props) {
     //const [,  ] = useState([]);
-    
+    const login = useLogin();
+    const nav = useNavigate();
+    useEffect(() => {
+        login.checkLogin(user => {
+            if (user) {
+                nav('/home');
+            }
+        });
+    }, [login, nav]);
+
     const init_state = {
         'password': '',
         'email': ''
     };
     const handleChange = (field) => (ev) => { setState({[field]: ev.target.value});}
     const [state, setState] = useBetterState(init_state);
-    const [showPass, setShowPass] = useState(false);
+    //const [showPass, setShowPass] = useState(false);
     
     return (
+        <div className="frame page-main login-page">
+        <div className="header">
+            <h1>
+                Recipe Book Login
+            </h1>
+        </div>
+        <div className="main" id="content">
+        <img src="./loginImage.jpg" alt="Cookbook Image" ></img>
+        
         <div className="loginNav frame-footer"> 
-            
             <div className = "loginComponent">
                 
                 <TextField
@@ -55,9 +68,13 @@ export default function Login(props) {
                     onChange={handleChange('password')}
 
                 />
-                <Button variant="outlined" onClick ={() => login.doLogin(state)}> Login</Button>    
+                <Button variant="outlined" onClick ={() => login.doLogin(state).then(_ => nav('/home'))}> Login</Button>    
             </div>
 
         </div>
+        </div>
+        
+    </div>
+        
     )
 }
